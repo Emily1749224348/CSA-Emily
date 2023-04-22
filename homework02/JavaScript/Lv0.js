ee51396c87eb91a2d48727b5d2cd52acaeb31352
Emily1749224348 <ruyi.jiang0301@qq.com>
2023-04-22 14:35:39 +0800

    homework02
    with Lv0 and Lv1

---
+++ homework02/JavaScript/Lv0.js
@@ -0,0 +1,35 @@
+//Lv0 使用三种不同方法创建一个对象
+// let a=new Object();
+// a.name="Ella";
+// a.age="44";
+// a.job="Teacher";
+// console.log(a);
+
+// let b={
+//     name:"Jack",
+//     age:30,
+//     job:"football player",
+// }
+// console.log(b);
+
+// function Personal_Indendity()
+// {
+//     this.name="Anne";
+//     this.age=40;
+//     this.job="office worker";
+// }
+// c=new Personal_Indendity();
+// console.log(c);
+
+//Lv1
+/*
+function mupush(arr,x)
+{
+    arr.push(x);
+}
+arr=[0];
+mupush(arr,1);
+console.log(arr);
+
+*/
+
